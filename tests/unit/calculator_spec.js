a58f1689505bd83581d8ad6837ee957e85a5b0f8
Evan Reid <evan_1995@hotmail.com>
2021-01-24 17:23:59 +0000

    add, subtract, multiply and divide complete - tests pass

--- tests/unit/calculator_spec.js
+++ tests/unit/calculator_spec.js
@@ -12,8 +12,27 @@ describe('calculator', function () {
   })
 
   it('it can add 1 to 4 and get 5', function () {
-    calculator.previousTotal = 1;
-    calculator.add(4);
+    calculator.previousTotal = 4;
+    calculator.add(1);
     assert.strictEqual(calculator.runningTotal, 5)
   } )
+
+  it('it can subtract 4 from 7 and get 3', function(){
+    calculator.previousTotal = 7;
+    calculator.subtract(4);
+    assert.strictEqual(calculator.runningTotal, 3)
+  })
+
+  it('it can multiply 3 by 5 and get 15', function(){
+    calculator.previousTotal = 3;
+    calculator.multiply(5);
+    assert.strictEqual(calculator.runningTotal, 15)
+  })
+
+  it('it can divide 21 by 7 and get 3', function(){
+    calculator.previousTotal = 21;
+    calculator.divide(7);
+    assert.strictEqual(calculator.runningTotal, 3)
+  })
+
 });
